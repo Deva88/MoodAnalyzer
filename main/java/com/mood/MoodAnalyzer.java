0c43ea68d79dbd7180b6ba4607513a1c66293e54
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-06 00:57:24 +0530

    Added: Respond Happy or Sad Mood

--- main/java/com/mood/MoodAnalyzer.java
+++ main/java/com/mood/MoodAnalyzer.java
@@ -1,6 +1,16 @@
 package com.mood;
-
+/*@Description:- analyse checking mood the message entered by user*/
 public class MoodAnalyzer {
-
+    /*@Description:- user mood by checking if the message given by user
+     * contains word sad or happy and return the mood value according to it
+     */
+    public String moodAnalyser(String message) {
+        if (message.contains("Happy") || message.contains("happy"))
+        {
+            return "HAPPY";
+        }
+        else
+            return "SAD";
+    }
 }
 
--- test/java/com/mood/MoodAnalyzerTest.java
+++ test/java/com/mood/MoodAnalyzerTest.java
@@ -1,7 +1,17 @@
 package com.mood;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyzerTest {
     MoodAnalyzer mood=new MoodAnalyzer();
+    @Test
+    public void given_Mood_When_HappySad_ReturnMessage() {
+        String moodTesting = mood.moodAnalyser("i am in Happy mood");
+        boolean result = moodTesting.contains("HAPPY");
+        System.out.println(result);
 
+        Assertions.assertEquals(true,result);
+    }
 }
 
