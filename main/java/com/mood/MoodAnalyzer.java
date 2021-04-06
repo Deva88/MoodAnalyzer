7a63c286e32649972586a7b01c58c3401778026c
Deva88 <devendra.raj.sdm@gmail.com>
2021-04-07 04:05:01 +0530

    Added: I am in any mood Return HAPPY

--- main/java/com/mood/MoodAnalyzer.java
+++ main/java/com/mood/MoodAnalyzer.java
@@ -2,12 +2,16 @@ package com.mood;
 /*@Description:- analyse checking mood the message entered by user*/
 public class MoodAnalyzer {
     /*@Description:- user mood by checking if the message given by user
-     * method can just Return SAD
+     * method (I am in any mood) can just Check for SAD else Return Happy
      */
     public String moodAnalyser(String message) {
+        if(message.contains("Sad"))
         {
             return "SAD";
         }
+        else
+            return "HAPPY";
     }
 }
+
 
--- test/java/com/mood/MoodAnalyzerTest.java
+++ test/java/com/mood/MoodAnalyzerTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyzerTest {
     MoodAnalyzer mood=new MoodAnalyzer();
+
     @Test
+    //TC1.2-validate (i am in any mood) should return happy
     public void given_Mood_When_HappySad_ReturnMessage() {
         MoodAnalyzer moodTesting =new MoodAnalyzer();
-        String mood= moodTesting.moodAnalyser("i am in sad mood");
-
-        Assertions.assertEquals("SAD",mood);
+        String mood= moodTesting.moodAnalyser("I am in any mood");
+        Assertions.assertEquals("HAPPY",mood);
     }
 }
 
